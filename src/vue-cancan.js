c712c6dd767c648529a393d71dcb9b07ab5d5882
Dale Zak <dalezak@gmail.com>
2020-05-14 19:24:00 -0600

    Added setRules method

--- src/vue-cancan.js
+++ src/vue-cancan.js
@@ -1,12 +1,12 @@
 function checkAction(abilityAction, checkedAction) {
   return (abilityAction == 'manage') ||
-         (abilityAction == checkedAction) ||
-         (abilityAction == 'read' && (checkedAction == 'index' || checkedAction == 'show'));
+    (abilityAction == checkedAction) ||
+    (abilityAction == 'read' && (checkedAction == 'index' || checkedAction == 'show'));
 }
 
 function checkSubject(abilitySubject, checkedSubject) {
   return (abilitySubject == 'all') ||
-         (abilitySubject == checkedSubject);
+    (abilitySubject == checkedSubject);
 }
 
 export default {
@@ -18,14 +18,20 @@ export default {
     }) && true;
   },
 
+  setRules(rules) {
+    this.rules = rules;
+  },
+
   install(Vue, options) {
     this.rules = options.rules;
 
     Vue.prototype.$can = (action, subject) => this.$can(action, subject);
 
+    Vue.prototype.setRules = (rules) => this.setRules(rules);
+
     Vue.directive('can', {
       inserted: (el, binding) => {
-        if(!this.$can(Object.keys(binding.modifiers)[0], Object.keys(binding.modifiers)[1])) {
+        if (!this.$can(Object.keys(binding.modifiers)[0], Object.keys(binding.modifiers)[1])) {
           el.remove();
         }
       }
@@ -36,7 +42,7 @@ export default {
     return (to, from, next) => {
       const subject = to.path.replace(/^\//, '').split('/')[0] || 'index';
       const action = to.path.replace(/^\//, '').split('/')[1] || 'index';
-      if(this.$can(action, subject)) {
+      if (this.$can(action, subject)) {
         next()
       } else {
         next(defaultPath);
